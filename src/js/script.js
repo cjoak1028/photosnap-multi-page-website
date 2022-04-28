b6d9f7cc5db564d2042ed803a91a2c2fa1f99ec4
David Kim <cjoak1028@gmail.com>
2022-04-28 13:38:47 -0400

    refactored header

--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,26 @@ const header = document.querySelector('.header');
 
 // Check whether browser is Safari or not
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 const mediaQueryList = window.matchMedia("screen and (min-width: 47.8em)");
 
+// Close mobile menu when it's open during transition from mobile to tablet screen
 if (isSafari) {
     mediaQueryList.addListener((e) => {
-        e.matches && header.classList.contains("show-menu") && toggleMenu();
+        e.matches && header.classList.contains("menu-drop") && toggleMenu();
     });
 } else {
     mediaQueryList.addEventListener("change", (e) => {
         console.log('hello!');
-        e.matches && header.classList.contains("show-menu") && toggleMenu();
+        e.matches && header.classList.contains("menu-drop") && toggleMenu();
     });
 };
 
 const toggleMenu = () => {
     hamburgerButton.classList.toggle('open');
-    header.classList.toggle('show-menu');
-}
+    header.classList.toggle('menu-drop');
+};
 
 hamburgerButton.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('open');
-    header.classList.toggle('show-menu');
-})
+    toggleMenu();
+});
