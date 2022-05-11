3fb143671b1fe87801a2f20452d72430e472dbee
David Kim <cjoak1028@gmail.com>
2022-05-10 22:32:26 -0400

    added selected class for header and footer links

--- dist/stories.e82f28a0.js
+++ dist/stories.e82f28a0.js
@@ -516,22 +516,28 @@ function hmrAcceptRun(bundle, id) {
 },{}],"dV6cC":[function(require,module,exports) {
 const hamburgerButton = document.querySelector('.hamburger-btn');
 const header = document.querySelector('.header');
+const homeLink = document.querySelector('.home-link');
+const storiesLinks = document.querySelectorAll('.stories-link');
+const featuresLinks = document.querySelectorAll('.features-link');
+const pricingLinks = document.querySelectorAll('.pricing-link');
 const planSwitch = document.querySelector('.plan-switch input');
 const switchLabels = document.querySelectorAll('.plan-switch__label');
 const basicPrice = document.querySelector('.basic-price');
 const proPrice = document.querySelector('.pro-price');
 const businessPrice = document.querySelector('.business-price');
 // Check whether browser is Safari or not
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-const mediaQueryList = window.matchMedia("screen and (min-width: 47.8em)");
+// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+// const mediaQueryList = window.matchMedia("screen and (min-width: 47.8em)");
 // Close mobile menu when it's open during transition from mobile to tablet screen
-if (isSafari) mediaQueryList.addListener((e)=>{
-    e.matches && header.classList.contains("menu-drop") && toggleMenu();
-});
-else mediaQueryList.addEventListener("change", (e)=>{
-    console.log('hello!');
-    e.matches && header.classList.contains("menu-drop") && toggleMenu();
-});
+// if (isSafari) {
+//     mediaQueryList.addListener((e) => {
+//         e.matches && header.classList.contains("menu-drop") && toggleMenu();
+//     });
+// } else {
+//     mediaQueryList.addEventListener("change", (e) => {
+//         e.matches && header.classList.contains("menu-drop") && toggleMenu();
+//     });
+// };
 const toggleMenu = ()=>{
     hamburgerButton.classList.toggle('open');
     header.classList.toggle('menu-drop');
@@ -539,20 +545,36 @@ const toggleMenu = ()=>{
 hamburgerButton.addEventListener('click', ()=>{
     toggleMenu();
 });
-planSwitch.addEventListener('change', ()=>{
-    switchLabels.forEach((label)=>{
-        label.classList.toggle('selected');
+const init = ()=>{
+    const currentURL = window.location.href;
+    if (currentURL.includes('pricing')) {
+        planSwitch.addEventListener('change', ()=>{
+            switchLabels.forEach((label)=>{
+                label.classList.toggle('selected');
+            });
+            if (planSwitch.checked) {
+                basicPrice.innerHTML = '$190.00';
+                proPrice.innerHTML = '$390.00';
+                businessPrice.innerHTML = '$990.00';
+            } else {
+                basicPrice.innerHTML = '$19.00';
+                proPrice.innerHTML = '$39.00';
+                businessPrice.innerHTML = '$99.00';
+            }
+        });
+        // Select header link
+        pricingLinks.forEach((link)=>{
+            link.classList.add('selected');
+        });
+    } else if (currentURL.includes('stories')) storiesLinks.forEach((link)=>{
+        link.classList.add('selected');
     });
-    if (planSwitch.checked) {
-        basicPrice.innerHTML = '$190.00';
-        proPrice.innerHTML = '$390.00';
-        businessPrice.innerHTML = '$990.00';
-    } else {
-        basicPrice.innerHTML = '$19.00';
-        proPrice.innerHTML = '$39.00';
-        businessPrice.innerHTML = '$99.00';
-    }
-});
+    else if (currentURL.includes('features')) featuresLinks.forEach((link)=>{
+        link.classList.add('selected');
+    });
+    else homeLink.classList.add('selected');
+};
+init();
 
 },{}]},["1B2dF","dV6cC"], "dV6cC", "parcelRequire6d81")
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
 const hamburgerButton = document.querySelector('.hamburger-btn');
 const header = document.querySelector('.header');
 
+const homeLink = document.querySelector('.home-link');
+const storiesLinks = document.querySelectorAll('.stories-link');
+const featuresLinks = document.querySelectorAll('.features-link');
+const pricingLinks = document.querySelectorAll('.pricing-link');
+
 const planSwitch = document.querySelector('.plan-switch input');
 const switchLabels = document.querySelectorAll('.plan-switch__label');
 
@@ -9,21 +14,20 @@ const proPrice = document.querySelector('.pro-price');
 const businessPrice = document.querySelector('.business-price');
 
 // Check whether browser is Safari or not
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-const mediaQueryList = window.matchMedia("screen and (min-width: 47.8em)");
+// const mediaQueryList = window.matchMedia("screen and (min-width: 47.8em)");
 
 // Close mobile menu when it's open during transition from mobile to tablet screen
-if (isSafari) {
-    mediaQueryList.addListener((e) => {
-        e.matches && header.classList.contains("menu-drop") && toggleMenu();
-    });
-} else {
-    mediaQueryList.addEventListener("change", (e) => {
-        console.log('hello!');
-        e.matches && header.classList.contains("menu-drop") && toggleMenu();
-    });
-};
+// if (isSafari) {
+//     mediaQueryList.addListener((e) => {
+//         e.matches && header.classList.contains("menu-drop") && toggleMenu();
+//     });
+// } else {
+//     mediaQueryList.addEventListener("change", (e) => {
+//         e.matches && header.classList.contains("menu-drop") && toggleMenu();
+//     });
+// };
 
 const toggleMenu = () => {
     hamburgerButton.classList.toggle('open');
@@ -34,18 +38,41 @@ hamburgerButton.addEventListener('click', () => {
     toggleMenu();
 });
 
-planSwitch.addEventListener('change', () => {
-    switchLabels.forEach(label => {
-        label.classList.toggle('selected');
-    });
+const init = () => {
+    const currentURL = window.location.href;
 
-    if (planSwitch.checked) {
-        basicPrice.innerHTML = '$190.00';
-        proPrice.innerHTML = '$390.00';
-        businessPrice.innerHTML = '$990.00';
+    if (currentURL.includes('pricing')) {
+        planSwitch.addEventListener('change', () => {
+            switchLabels.forEach(label => {
+                label.classList.toggle('selected');
+            });
+
+            if (planSwitch.checked) {
+                basicPrice.innerHTML = '$190.00';
+                proPrice.innerHTML = '$390.00';
+                businessPrice.innerHTML = '$990.00';
+            } else {
+                basicPrice.innerHTML = '$19.00';
+                proPrice.innerHTML = '$39.00';
+                businessPrice.innerHTML = '$99.00';
+            }
+        });
+
+        // Select header link
+        pricingLinks.forEach(link => {
+            link.classList.add('selected');
+        });
+    } else if (currentURL.includes('stories')) {
+        storiesLinks.forEach(link => {
+            link.classList.add('selected');
+        });
+    } else if (currentURL.includes('features')) {
+        featuresLinks.forEach(link => {
+            link.classList.add('selected');
+        });
     } else {
-        basicPrice.innerHTML = '$19.00';
-        proPrice.innerHTML = '$39.00';
-        businessPrice.innerHTML = '$99.00';
+        homeLink.classList.add('selected');
     }
-});
+}
+
+init();
